629db3e6b19ba59e13d688e12bc17bafeff7a048
sspork <sporksa.student@waldorfmaui.org>
2020-03-05 14:44:53 -1000

    first working version

--- game.ts
+++ game.ts
@@ -4,33 +4,48 @@ import { words } from "./words";
 
 const word = words[Math.floor(Math.random() * words.length)]
 const parts = word.split("")
+let failsCounter = 0
+const maxFails = 3
 
 const guessedLetters = []
 async function main() {
     while (true) {
-        printCurrentState()
+        const wordGuessed = printCurrentState()
+        if (wordGuessed) {
+            console.log("yes, the word has been guessed")
+            break
+        }
         const letter = await askForLetter()
         if (parts.includes(letter)) {
             console.log("yes :)")
             guessedLetters.push(letter)
         } else {
-            console.log("no :(")
+            failsCounter++
+            if (failsCounter > maxFails) {
+                console.log("you lost.")
+                break
+            }
+            console.log("no :( you have " + (maxFails - failsCounter) + " fails left")
+
+
         }
     }
 }
 function printCurrentState() {
     let stuff = "";
+    let wordGuessed = true
 
     for (let i = 0; i < word.length; i++) {
         if (guessedLetters.includes(parts[i])) {
             stuff += parts[i]
         } else {
             stuff += "_"
-
+            wordGuessed = false
         }
         stuff += " "
     }
     console.log(stuff)
+    return wordGuessed
 }
 
 async function askForLetter() {
